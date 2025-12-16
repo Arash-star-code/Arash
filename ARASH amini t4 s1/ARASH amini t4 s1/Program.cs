25ca4c91de007b72bf64570aa056a403e1c12a67
Arash-star-code <arashamini6985@gmail.com>
2025-12-16 15:15:56 -0500

    Add files via upload

---
+++ ARASH amini t4 s1/ARASH amini t4 s1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int even = 0, odd = 0;
+        Console.WriteLine("10 enter num:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"adad {i}: ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                if (n % 2 == 0) even++;
+                else odd++;
+            }
+            else
+            {
+                Console.WriteLine("dubare talash kon.");
+                i--;
+            }
+        }
+        Console.WriteLine($"tedad adade zoj: {even}");
+        Console.WriteLine($"tedad adade fard: {odd}");
+    }
+}
+
