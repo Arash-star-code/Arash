54408a3278574fabff7d7ebb9607d6bc2c874604
Arash-star-code <arashamini6985@gmail.com>
2025-12-16 15:17:53 -0500

    Add files via upload

---
+++ ARASH amini t5 s2/ARASH amini t5 s2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = Multiply(a, b);
+        Console.WriteLine($"Result = {result}");
+    }
+
+    static int Multiply(int a, int b)
+    {
+        if (b == 0)
+            return 0;
+
+        return a + Multiply(a, b - 1);
+    }
+}
