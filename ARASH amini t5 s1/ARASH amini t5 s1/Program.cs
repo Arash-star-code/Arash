359cb806e66b16ae83f0589e3bcbbca4d875b216
Arash-star-code <arashamini6985@gmail.com>
2025-12-16 15:17:12 -0500

    Add files via upload

---
+++ ARASH amini t5 s1/ARASH amini t5 s1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ARASH_amini_t5_s1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
