c3936967f4682a6cca349b2ffb0b331588d185d1
Arash-star-code <arashamini6985@gmail.com>
2025-12-16 15:16:35 -0500

    Add files via upload

---
+++ ARASH amini t4 s2/ARASH amini t4 s2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool found = false;
+        string positions = "";
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"adad ro bede {i}: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 2)
+            {
+                found = true;
+                positions += i + " ";
+            }
+        }
+
+        if (found)
+        {
+            Console.WriteLine("adad 2 hast.");
+            Console.WriteLine("adad 2 tu pele haye : " + positions);
+        }
+        else
+        {
+            Console.WriteLine("adad 2 nist.");
+        }
+    }
+}
